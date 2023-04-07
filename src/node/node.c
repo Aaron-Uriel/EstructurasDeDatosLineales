770ef6faf2e679ec4303828197e0628412d34e35
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-04-07 17:24:10 -0600

    Añadir definición de nodos. No está probada todavía, pero **probablemente** sirve xd.

---
+++ src/node/node.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+#include <node.h>
+
+struct Node {
+    int32_t value;
+    Node    *_previous;
+    Node    *_next;
+};
+
+Node *
+node_new(int32_t value)
+{
+	Node *const new_node = malloc(sizeof(Node));
+	if (new_node != NULL) {
+		new_node->value     = value;
+		new_node->_next     = NULL;
+        new_node->_previous = NULL;
+	}
+
+	return new_node;
+}
+
+void
+node_delete(Node *const self)
+{
+	free(self);
+}
+
+Node *
+node_get_next(Node *const self)
+{
+	return self->_next;
+}
+
+Node
+*node_get_previous(Node *const self)
+{
+    return self->_previous;
+}
+
+void 
+node_append(Node *const self, Node *const adjacent_node)
+{
+	self->_next = adjacent_node;
+}
+
+void
+node_prepend(Node *const self, Node *const adjacent_node)
+{
+    self->_previous = adjacent_node;
+}
---
+++ src/node/snode.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+#include <node.h>
+
+struct SNode {
+	int32_t  value;
+	SNode    *_next;
+};
+
+SNode *
+snode_new(int32_t value)
+{
+	SNode *const new_snode = malloc(sizeof(SNode));
+	if (new_snode != NULL) {
+		new_snode->value     = value;
+		new_snode->_next     = NULL;
+	}
+
+	return new_snode;
+}
+
+void
+snode_delete(SNode *const self)
+{
+	free(self);
+}
+
+SNode *
+snode_get_next(SNode *const self)
+{
+	return self->_next;
+}
+
+void 
+snode_append(SNode *const self, SNode *const adjacent_snode)
+{
+	self->_next = adjacent_snode;
+}
+

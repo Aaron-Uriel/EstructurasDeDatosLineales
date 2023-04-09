7766e953dc1467304bbc26a4d9949f7d97500654
ferfabianreyes <2100721a@umich.mx>
2023-04-09 15:57:16 -0500

    Hice el menu principal de como están ligados

--- tests/test.c
+++ tests/test.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
-
 #include <libdstruct.h>
+
+int menu_principal(void)
+{
+    int opcion;
+    printf("._____________________________.\n");
+    printf("|            Menu.            |\n");
+    printf("|-----------------------------|\n");
+    printf("|1.- Simplemente ligadas.     |\n");
+    printf("|2.- Doblemmente ligadas.     |\n");
+    printf("|3.- Con cabecera.            |\n");
+    printf("|4.- Salir.                   |\n");
+    printf("|_____________________________|\n");
+    printf("|Opcion: ");
+    scanf("%d", &opcion);
+    printf("|_____________________________|\n");
+    return opcion;
+}
 
 int main(void)
 {
-    printf("Holaaaaa!\n"
-           "Me compilaste com exito xd, Felicidades!, yeii, yeii2\n");
+    int opcion;
+    opcion = menu_principal();
+    if (opcion == 0)
+    {
+        printf("Fue 0");
+    }
+    
 
     return 0;
 }

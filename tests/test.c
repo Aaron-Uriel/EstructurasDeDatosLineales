0119228ad02cd34cb99649f8e28bc2f8a57a0225
ferfabianreyes <2100721a@umich.mx>
2023-04-09 21:57:00 -0500

    Estrucutura del menú secundario.

--- tests/test.c
+++ tests/test.c
@@ -4,6 +4,8 @@
 int menu_principal(void)
 {
     int opcion;
+    printf("*******************************\n");
+    printf("|   Colas, Pilas y listas.    |\n");
     printf("._____________________________.\n");
     printf("|            Menu.            |\n");
     printf("|-----------------------------|\n");
@@ -18,11 +20,32 @@ int menu_principal(void)
     return opcion;
 }
 
+int menu_secundario(void)
+{
+    int opcionSecun;
+    printf("\t|____________.\n");
+    printf("\t|1.- Cola.   |\n");
+    printf("\t|2.- Pila.   |\n");
+    printf("\t|3.- Lista.  |\n");
+    printf("\t|4.- Volver. |\n");
+    printf("\t|____________|\n");
+     printf("\t|Opcion: ");
+    scanf("%d", &opcionSecun);
+    printf("\t|_____________|\n");
+    return opcionSecun;
+}
+
 int main(void)
 {
-    int opcion;
+    int opcion, opcionSecun;
     opcion = menu_principal();
     if (opcion == 0)
+    {
+        printf("Fue 0");
+    }
+
+    opcionSecun = menu_secundario();
+    if (opcionSecun == 0)
     {
         printf("Fue 0");
     }

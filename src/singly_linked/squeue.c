3a3d940a35c058f4b70c8db598c759490699c6eb
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-04-10 13:51:26 -0600

    Añadir squeue.c junto con sus funciones.

---
+++ src/singly_linked/squeue.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <libdstruct.h>
+
+SNode *
+squeue_extract_node(SNode **const squeue)
+{
+    /* squeue solo  es NULL si la cola está vacía */
+    if (squeue == NULL) {
+        return NULL;
+    } else {
+        SNode *const extracted_node = *squeue;
+        *squeue = (*squeue)->next;
+        return extracted_node; 
+    }
+}
+
+void
+squeue_insert_node(SNode **squeue, SNode *snode)
+{
+    /* squeue solo es NULL cuando la cola está vacía. */
+    if (squeue == NULL) {
+        squeue = &snode;
+    } else {
+        SNode *const last_node_in_squeue = snode_jump_to_end(*squeue);
+        last_node_in_squeue->next = snode;
+    }
+}
+
+void
+squeue_imprimir(const SNode *const *const squeue)
+{
+    snode_print_all_linked_nodes(*squeue);
+}
+

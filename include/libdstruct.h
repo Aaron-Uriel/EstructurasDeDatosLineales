17ff0b47e282b1f85bf08687012e9a22e5424e6a
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-04-11 19:43:12 -0600

    Hacer todas las funciones públicas. Se me olvidó antes hacerlas públicas, por lo que causaban errores de funciones no definidas para el enlazador.

--- include/libdstruct.h
+++ include/libdstruct.h
@@ -29,14 +29,14 @@ struct SNode {
 	SNode    *next;
 };
 
-SNode   *snode_new(int32_t value);
-void    snode_delete(SNode **self);
-SNode   *snode_get_next(SNode *const self);
-void    snode_append(SNode *const self, SNode *const adjacent_node);
-SNode   *snode_jump_to_last(SNode *const self);
-SNode   *snode_jump_to_n(SNode *const self, const uint32_t n);
-void    snode_print_all_linked_nodes(const SNode *const self);
-void    snode_print_debug(const SNode *const self);
+PUBLIC SNode   *snode_new(int32_t value);
+PUBLIC void    snode_delete(SNode **self);
+PUBLIC SNode   *snode_get_next(SNode *const self);
+PUBLIC void    snode_append(SNode *const self, SNode *const adjacent_node);
+PUBLIC SNode   *snode_jump_to_last(SNode *const self);
+PUBLIC SNode   *snode_jump_to_n(SNode *const self, const uint32_t n);
+PUBLIC void    snode_print_all_linked_nodes(const SNode *const self);
+PUBLIC void    snode_print_debug(const SNode *const self);
 
 typedef struct Node Node;
 struct Node {
@@ -45,27 +45,29 @@ struct Node {
     Node    *next;
 };
 
-Node *node_new(int32_t value);
-void node_delete(Node **const self);
-Node *node_get_next(Node *const self);
-Node *node_get_previous(Node *const self);
-void node_append(Node *const self, Node *const adjacent_node);
-void node_prepend(Node *const self, Node *const adjacent_node);
-Node *node_jump_to_last(Node *const self);
-Node *node_jump_to_first(const Node *const self);
-Node *node_jump_to_n(Node *const self, const uint32_t n);
-void node_print_all_linked_nodes(Node *const self);
-void node_print_debug(const Node *const self);
+PUBLIC Node *node_new(int32_t value);
+PUBLIC void node_delete(Node **const self);
+PUBLIC Node *node_get_next(Node *const self);
+PUBLIC Node *node_get_previous(Node *const self);
+PUBLIC void node_append(Node *const self, Node *const adjacent_node);
+PUBLIC void node_prepend(Node *const self, Node *const adjacent_node);
+PUBLIC Node *node_jump_to_last(Node *const self);
+PUBLIC Node *node_jump_to_first(Node *const self);
+PUBLIC Node *node_jump_to_n(Node *const self, const uint32_t n);
+PUBLIC void node_print_all_linked_nodes(const Node *const self);
+PUBLIC void node_print_debug(const Node *const self);
 
 /* Fin de nodos. */
 
 /* Listas. */
 
-void    slist_insert_node(SNode **const slist, SNode *const node, const uint32_t index);
-SNode   *slist_extract_node(SNode **const slist, const uint32_t index);
+PUBLIC void    slist_insert_node(SNode **const slist, SNode *const node, const uint32_t index);
+PUBLIC SNode   *slist_extract_node(SNode **const slist, const uint32_t index);
 void    slist_print(SNode **slist);
 
 typedef struct HList HList;
+
+PUBLIC void slist_push_back(SNode **slist, SNode *snode);
 
 /* Fin de listas. */
 
@@ -77,7 +79,7 @@ typedef struct HQueue HQueue;
 
 /* Pilas. */
 
-void add_node (SNode *pila, SNode node);
+PUBLIC void add_node (SNode *pila, SNode node);
 
 /* Fin de pilas. */
 

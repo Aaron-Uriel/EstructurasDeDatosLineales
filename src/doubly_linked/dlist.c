51718bc63f803cc48316675f088b013d469511a4
ferfabianreyes <2100721a@umich.mx>
2023-04-19 22:50:46 -0600

    agregar y acomodar nodo en lista doblemente ligada probada y terminada

--- src/doubly_linked/dlist.c
+++ src/doubly_linked/dlist.c
@@ -3,11 +3,12 @@
 
 #include <libdstruct.h>
 
-void dlist_insert_node(Node **dlist, Node *node, int id){
+void dolist_insert_node(Node **dlist, Node *node, int id)
+{
+    int listo = 0, cont = 0;
     node = node_new(id); 
     Node *actual = *dlist;
-    Node *prev; 
-
+    Node *prev, *temporal = node; 
 
     if (node != NULL)
     {
@@ -16,19 +17,61 @@ void dlist_insert_node(Node **dlist, Node *node, int id){
 
         } else {
             actual = *dlist; 
-            while (actual->next != NULL) 
-            {;
-                prev = actual;
-                actual = actual->next;
-                actual->previous = prev;
+            while (actual->next != NULL && listo == 0) 
+            {
+                if (node->value > actual->value)
+                {
+                    cont ++;
+                    prev = actual;
+                    actual = actual->next;
+                    actual->previous = prev;
 
+                    if (actual->next == NULL && node->value <= actual->value)
+                    {
+                        temporal = actual;
+                        node->previous = prev;
+                        actual->previous = node;
+                        node->next = temporal;
+                        prev->next = node;
+                        listo = 1;
+                    }
+                    
+
+                } else if (node->value <= actual->value)
+                {
+                    
+                    if (cont == 0)
+                    {
+                        temporal = actual;
+                        node->previous = NULL;
+                        actual->previous = node;
+                        node->next = temporal;
+                        *dlist = node;
+
+                    } else {
+                        temporal = actual;
+                        node->previous = prev;
+                        actual->previous = node;
+                        node->next = temporal;
+                        prev->next = node;
+                    }
+                
+                    listo = 1;
+
+                }
+                
             }
 
+            if (listo < 1)
+            {
             actual->next = node;
             node->previous = actual;
-
-            
+            node->next = NULL;
+            }
+ 
         }
+        
+
     }
 
 }
--- tests/stack.c
+++ tests/stack.c
@@ -5,11 +5,10 @@
 
 
 void dolist_insert_node(Node **dlist, Node *node, int id){
-    int listo = 0;
+    int listo = 0, cont = 0;
     node = node_new(id); 
     Node *actual = *dlist;
     Node *prev, *temporal = node; 
-
 
     if (node != NULL)
     {
@@ -20,21 +19,43 @@ void dolist_insert_node(Node **dlist, Node *node, int id){
             actual = *dlist; 
             while (actual->next != NULL && listo == 0) 
             {
-                if (node->value >= actual->value)
+                if (node->value > actual->value)
                 {
+                    cont ++;
                     prev = actual;
                     actual = actual->next;
                     actual->previous = prev;
 
-                } else if (node->value < actual->value)
-                {
-                    temporal = actual;
-                    node->previous = temporal->previous;
-                    actual->previous = node;
-                    node->next = actual;
-                    prev->next = node;
-                    *dlist = node;
+                    if (actual->next == NULL && node->value <= actual->value)
+                    {
+                        temporal = actual;
+                        node->previous = prev;
+                        actual->previous = node;
+                        node->next = temporal;
+                        prev->next = node;
+                        listo = 1;
+                    }
                     
+
+                } else if (node->value <= actual->value)
+                {
+                    
+                    if (cont == 0)
+                    {
+                        temporal = actual;
+                        node->previous = NULL;
+                        actual->previous = node;
+                        node->next = temporal;
+                        *dlist = node;
+
+                    } else {
+                        temporal = actual;
+                        node->previous = prev;
+                        actual->previous = node;
+                        node->next = temporal;
+                        prev->next = node;
+                    }
+                
                     listo = 1;
 
                 }
@@ -52,9 +73,6 @@ void dolist_insert_node(Node **dlist, Node *node, int id){
         
 
     }
-
-
-
 
 }
 

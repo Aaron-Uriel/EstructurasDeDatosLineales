f65fb11710ddeb4a3ceefe249794ef01b16aa69d
ferfabianreyes <2100721a@umich.mx>
2023-04-20 00:31:23 -0600

    Archivo de dlist.c terminado

--- src/doubly_linked/dlist.c
+++ src/doubly_linked/dlist.c
@@ -79,3 +79,53 @@ void dlist_imprimir(Node **list)
 {
     node_print_all_linked_nodes(*list);
 }
+
+void delete_node(Node **dlist, int id) //Elimina por medio de id y sólo el primero.
+{
+    Node *actual = *dlist, *prev = NULL, *next = NULL;
+    int deleted, cont = 0;
+    if (actual != NULL)
+    {
+        while (actual->next != NULL)
+        {
+            if (actual->value == id)
+            {
+                break;
+            } else
+            {
+                cont ++;
+                actual = actual->next;
+                
+            }
+        }
+        if (actual->value != id)
+        {
+            printf("No se encontro el nodo\n");
+        } else{
+            deleted = actual->value;
+            prev = actual->previous;
+            next = actual->next;
+            if (prev != NULL)
+            {
+                prev->next = next;
+            }
+            
+            
+            if (next != NULL)
+            {
+                next->previous = prev;
+            }
+            actual->previous = NULL;
+            actual->next = NULL;
+            free(actual);
+            printf("Se eliminó: %d \n", deleted);
+            if (cont == 0)
+            {
+                *dlist = next;
+            }
+            
+
+        }
+ 
+    }
+}

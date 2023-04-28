1125d9637f7746776a72af774f592c56cd99d4d6
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-04-28 10:30:18 -0600

    Corregir comportamiento de la pila estática.

---
+++ tests/static_stack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <libdstruct.h>
+
+int
+main(void) {
+    StaticStack *my_static_stack = static_stack_new(10);
+    int32_t i;
+    for (i = 0; i < 10; i++) {
+        static_stack_insert(my_static_stack, i * 5);
+    }
+    printf("%d\n", static_stack_extract(my_static_stack));
+    static_stack_print(my_static_stack);
+}

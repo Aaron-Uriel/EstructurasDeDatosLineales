e55257d0b7bdfc65ec39ac02bfed92a86bc7d39f
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-07-01 10:38:51 -0600

    Modificaciones de la pila.

--- src/singly_linked/sstack.c
+++ src/singly_linked/sstack.c
@@ -4,69 +4,39 @@
 
 
 /*
-Función que no regresa nada, recibe la dirección de
-memoria de un SNode * y un puntero de tipo SNode.
-Si el SNode no es NULL: Si la pila == NULL, el SNode se 
-pone como primero, sino, se va hasta el último y lo inserta.
-*/
-void sstack_insert_node(SNode **sstack, SNode *snode)
+ * Inserta un nodo en la pila en el primer lugar (el tope de la pila).
+ */
+void
+sstack_insert_node(SNode **pila, SNode *nodo)
 {
-    SNode *actual;
-
-    if (snode != NULL)
-    {
-        if (*sstack == NULL) {
-        *sstack = snode;
-        } else {
-            actual = *sstack; 
-            while (actual->next != NULL) 
-            {
-                actual = actual->next; 
-            }
-
-            actual->next = snode;
-
+    if (pila) {
+        if (*pila) {
+            nodo->next = *pila;
         }
+        *pila = nodo;
     }
 }
 
 /*
-Regresa un SNode * y recibe la dirección de memoria de un SNode *
-Crea dos SNode *, uno para el último y otro para el penúltimo
-Si la lista está vacía regresa un SNode * == NULL 
-Mientras el siguiente del último sea distinto de NULL:
-El penúltimo será el último y último será el SNdode * siguiente.
-Cuando se encuetre, sólo el penúltimo deja de apuntar al último 
-y regresa el nodo extraido.
-*/
-SNode *sstack_delete(SNode **sstack)
+ * Extrae el primer nodo de la pila (el de más arriba) y también lo desenlaza.
+ */
+SNode *
+sstack_delete(SNode **pila)
 {
-    SNode *second_last_snode = *sstack, *deleted = NULL;
-    SNode *last_snode = second_last_snode->next;
-
-    if (*sstack == NULL)
-    {
-        return deleted;
+    SNode *nodo_extraido = NULL;
+    if (pila && *pila) {
+        nodo_extraido = *pila;
+        *pila = (*pila)->next;
     }
-    
-    if (last_snode == NULL) {
-        sstack = NULL;
-    }
-
-    while(last_snode->next != NULL) {
-        second_last_snode = last_snode;
-        last_snode = last_snode->next;
-    }
-
-    deleted = last_snode;
-    last_snode = NULL;
-    second_last_snode->next = NULL;
-    return deleted;
+    return nodo_extraido;
 }
 
-//Imprime la pila.
-void sstack_imprimir(SNode **sstack)
+/*
+ * Imprime todos los nodos dentro de la pila.
+ */
+void
+sstack_imprimir(SNode **pila)
 {
-    snode_print_all_linked_nodes(*sstack);
+    snode_print_all_linked_nodes(*pila);
 }
 
